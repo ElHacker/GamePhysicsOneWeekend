8019a9aa8f7c2b1b6fa643dd7d0c86caf42c0c3a
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2024-04-03 20:55:16 -0700

    Add friction computations

--- code/Physics/Body.h
+++ code/Physics/Body.h
@@ -24,8 +24,10 @@ public:
 	Quat		m_orientation;
 	Vec3		m_linearVelocity;
 	Vec3		m_angularVelocity;
+
 	float		m_invMass;
 	float		m_elasticity;
+	float		m_friction;
 	Shape *		m_shape;
 
 	Vec3 GetCenterOfMassWorldSpace() const;
--- code/Physics/Contact.cpp
+++ code/Physics/Contact.cpp
@@ -46,12 +46,46 @@ void ResolveContact( contact_t & contact ) {
 	bodyA->ApplyImpulse(ptOnA, vectorImpulseJ * -1.0f);
 	bodyB->ApplyImpulse(ptOnB, vectorImpulseJ * 1.0f);
 
+	//
+	// Calculate the impulse caused by friction
+	//
+
+	const float frictionA = bodyA->m_friction;
+	const float frictionB = bodyB->m_friction;
+	const float friction = frictionA * frictionB;
+
+	// Find the normal direction of the velocity with respect to the normal of the coliision
+	const Vec3 velNorm = n * n.Dot(vab);
+
+	// Find the tangent direction of the velocity with respect to the normal of the collision
+	const Vec3 velTang = vab - velNorm;
+
+
+	// Get the tangential velocities relative to the other body
+	Vec3 relativeVelTang = velTang;
+	relativeVelTang.Normalize();
+
+	const Vec3 inertiaA = (invWorldInertiaA * ra.Cross(relativeVelTang)).Cross(ra);
+	const Vec3 inertiaB = (invWorldInertiaB * ra.Cross(relativeVelTang)).Cross(rb);
+	const float invInertia = (inertiaA + inertiaB).Dot(relativeVelTang);
+
+	// Calculate the tangential impulse for friction
+	const float reducedMass = 1.0f / (bodyA->m_invMass + bodyB->m_invMass + invInertia);
+	const Vec3 impulseFriction = velTang * reducedMass * friction;
+
+	// Apply kinetic friction
+	bodyA->ApplyImpulse(ptOnA, impulseFriction * -1.0f);
+	bodyB->ApplyImpulse(ptOnB, impulseFriction * 1.0f);
+
+	//
 	// Let's also move our colliding objects to just outside of each other.
 	// This is called "Projection Method"
+	//
+	const Vec3 ds = ptOnB - ptOnA;
+
 	const float tA = bodyA->m_invMass / (bodyA->m_invMass + bodyB->m_invMass);
 	const float tB = bodyB->m_invMass / (bodyA->m_invMass + bodyB->m_invMass);
 
-	const Vec3 ds = contact.ptOnB_WorldSpace - contact.ptOnA_WorldSpace;
 	bodyA->m_position += ds * tA;
 	bodyB->m_position -= ds * tB;
 }
